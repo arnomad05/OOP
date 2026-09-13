807a74f30979521eb459cd9195ad036955a46315
arnomad05 <imykhelov07@gmail.com>
2026-09-13 20:08:04 +0700

    Task_1_1_1

---
+++ Task_1_1_1/src/test/java/ru/nsu/aimykhelov/Task_1_1_1/SortTest.java
@@ -0,0 +1,52 @@
+package ru.nsu.aimykhelov.Task_1_1_1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SortTest {
+
+    @Test
+    void taskExample() {
+        int[] arr = {5, 4, 3, 2, 1};
+        Sort.sort(arr);
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5}, arr);
+    }
+
+    @Test
+    void alreadySorted() {
+        int[] arr = {1, 2, 3, 4, 5};
+        Sort.sort(arr);
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5}, arr);
+    }
+
+    @Test
+    void reverseSorted() {
+        int[] arr = {9, 7, 5, 3, 1};
+        Sort.sort(arr);
+        assertArrayEquals(new int[]{1, 3, 5, 7, 9}, arr);
+    }
+
+    @Test
+    void allEqual() {
+        int[] arr = {4, 4, 4, 4, 4};
+        Sort.sort(arr);
+        assertArrayEquals(new int[]{4, 4, 4, 4, 4}, arr);
+    }
+
+    @Test
+    void emptyArray() {
+        int[] arr = {};
+        Sort.sort(arr);
+        assertArrayEquals(new int[]{}, arr);
+    }
+
+    @Test
+    void singleElement() {
+        int[] arr = {42};
+        Sort.sort(arr);
+        assertArrayEquals(new int[]{42}, arr);
+    }
+
+
+}
