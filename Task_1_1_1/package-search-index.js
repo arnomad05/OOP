58f4913c4c3d89758cedda09acf93975b407ec79
arnomad05 <arnomad05@users.noreply.github.com>
2026-09-16 04:30:29 +0000

    Deploying to gh-pages from @ arnomad05/OOP@854e83c1c186798eabed790a4c1275e4d133a6f3 🚀

--- Task_1_1_1/package-search-index.js
+++ Task_1_1_1/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.aimykhelov.Task_1_1_1"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.aimykhelov.task111"}];updateSearchResults();
--- Task_1_1_1/type-search-index.js
+++ Task_1_1_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.aimykhelov.Task_1_1_1","l":"Dif"},{"p":"ru.nsu.aimykhelov.Task_1_1_1","l":"Main"},{"p":"ru.nsu.aimykhelov.Task_1_1_1","l":"Sort"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.aimykhelov.task111","l":"Dif"},{"p":"ru.nsu.aimykhelov.task111","l":"Main"},{"p":"ru.nsu.aimykhelov.task111","l":"Sort"}];updateSearchResults();
