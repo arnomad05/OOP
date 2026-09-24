112b92d9cc937cb34b84ae687cd343d5ace6b762
Artem Imykhelov <a.imykhelov@g.nsu.ru>
2026-09-25 02:06:45 +0700

    Fixed

--- Task_1_1_2/src/main/java/ru/nsu/aimykhelov/task112/Deck.java
+++ Task_1_1_2/src/main/java/ru/nsu/aimykhelov/task112/Deck.java
@@ -1,6 +1,11 @@
 package ru.nsu.aimykhelov.task112;
 
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.List;
+import java.util.Scanner;
 
 /**
  * Колода карт. Может состоять из одной или нескольких стандартных
